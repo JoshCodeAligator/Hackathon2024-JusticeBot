1d37fb2157ca7698a94083f8696fec289126648b
esosecn0 <christinenwoko@gmail.com>
2024-11-09 22:41:24 -0700

    dictionary

---
+++ advice_dictionary.js
@@ -0,0 +1,37 @@
+const advice = {
+  "workplace discrimination": {
+    "rights":"",
+      "advice": [
+        "File a complaint with the Alberta Human Rights Commission",
+          "Report incidents to the Human Rights Commission.",
+          "Seek legal counsel to discuss possible compensation or penalties.",
+          "Inform your employer or fellow employees of the situation"
+      ]
+  },
+  "police harassment": {
+      "advice": [
+          "File a complaint with the police oversight agency.",
+          "Contact a civil rights attorney to explore legal actions."
+      ]
+  },
+  "wage theft": {
+    "rights": [
+      "An employer cannot withold pay for more than a month",
+    "An employer must pay at least the mimimum wage rate ($15/hr in Alberta)",
+    "An employer cannot deduct earnings without consent, unless if accounting for overpay or vacation days, or given special permission from",
+  ],
+    
+    "advice": [
+        "File a complaint with the Employment Standards Branch.",
+        "Consult with a labor lawyer if you cannot resolve the issue with your employer."
+    ]
+},
+"limited access to healthcare": {
+  "advice": [
+      "File a complaint with the Employment Standards Branch.",
+      "Consult with a labor lawyer if you cannot resolve the issue with your employer."
+  ]
+}
+};
+  
+
