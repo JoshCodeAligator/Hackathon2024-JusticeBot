49bf977a1285b32a04775c440b2b140bfcfd1a03
esosecn0 <christinenwoko@gmail.com>
2024-11-09 23:16:00 -0700

    workplace

--- advice_dictionary.js
+++ advice_dictionary.js
@@ -1,6 +1,11 @@
 const advice = {
   "workplace discrimination": {
-    "rights":"",
+    "rights":[
+      "An employer cannot deny/terminate your employment due to race, religious beliefs, colour, gender, gender identity, gender expression, physical disability, mental disability, age, ancestry, place of origin, marital status, source of income, family status or sexual orientation"
+    ,"An employer cannot sanction you for reporting an incident",
+  "An employer must do as much as is in their power to accomodate to an individuals needs according to the disabilites act",
+"An employer must pay of both sexes the same rate for the same work",
+"An employer must ensure that the work environment is free from harmful discrimination, bullying, and harassment, and they must provide safety and health resources to all employees, including mental health services when necessary"],
       "advice": [
         "File a complaint with the Alberta Human Rights Commission",
           "Report incidents to the Human Rights Commission.",
@@ -19,8 +24,8 @@ const advice = {
       "An employer cannot withold pay for more than a month",
     "An employer must pay at least the mimimum wage rate ($15/hr in Alberta)",
     "An employer cannot deduct earnings without consent, unless if accounting for overpay or vacation days, or given special permission from",
+    "An employer must pay of both sexes the same rate for the same work"
   ],
-    
     "advice": [
         "File a complaint with the Employment Standards Branch.",
         "Consult with a labor lawyer if you cannot resolve the issue with your employer."
@@ -34,4 +39,7 @@ const advice = {
 }
 };
   
-
+  console.log(dictionary["landlord"]); // Accesses landlord data
+  console.log(dictionary["tenant"]);   // Accesses tenant data
+  console.log(dictionary["traffic"]);  // Accesses traffic data
+  
